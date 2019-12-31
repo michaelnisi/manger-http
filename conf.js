da5d038cfd942c5b5f21c1764716c14cdc34bb3f
Michael Nisi <michael.nisi@gmail.com>
2019-12-31 17:50:41 +0100

    Replace Bunyan logging with Pino

--- conf.js
+++ conf.js
@@ -1,27 +1,9 @@
 // config - configure manger-http
 
-const bunyan = require('bunyan');
+const pino = require('pino');
 
 // Working around https://github.com/nodejs/node/issues/16196
 require('tls').DEFAULT_ECDH_CURVE = 'auto';
-
-function level(l) {
-  return [10, 20, 30, 40, 50, 60].includes(l) ? l : null;
-}
-
-function log() {
-  const l = level(parseInt(process.env.MANGER_LOG_LEVEL, 10));
-
-  if (!l) {
-    return null;
-  }
-
-  return bunyan.createLogger({
-    name: 'manger',
-    level: l,
-    serializers: bunyan.stdSerializers,
-  });
-}
 
 const MAX_UPDATES = 8.64e7;
 
@@ -31,9 +13,13 @@ function maxUpdates() {
   return isNaN(max) ? MAX_UPDATES : max;
 }
 
+function createLogger() {
+  return pino({level: process.env.MANGER_LOG_LEVEL || 'info'});
+}
+
 exports.cacheSize = Number(process.env.LEVEL_DB_CACHE_SIZE);
 exports.location = process.env.LEVEL_DB_LOCATION;
-exports.log = log();
+exports.log = createLogger();
 exports.maxUpdates = maxUpdates();
 exports.port = parseInt(process.env.PORT, 10);
 
